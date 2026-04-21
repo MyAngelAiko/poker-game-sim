fd180edce23ff38db26372d2a8cf2eae90202843
Aiko <meramiroxy@gmail.com>
2026-04-21 16:03:14 +0300

    added structs/enum for easier hand comparisons (not finished)

--- main.cpp
+++ main.cpp
@@ -4,18 +4,63 @@
 #include <random>
 #include <algorithm>
 #include <bits/std_thread.h>
+enum class Suit {
+    Hearts,
+    Clubs,
+    Diamonds,
+    Spades
+};
+
+enum class Rank {
+    Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+};
+
+struct Card {
+    Suit suit;
+    Rank rank;
+};
+
+std::string suitToString(Suit suit) {
+    switch (suit) {
+        case Suit::Hearts: return "Hearts";
+        case Suit::Clubs: return "Clubs";
+        case Suit::Diamonds: return "Diamonds";
+        case Suit::Spades: return "Spades";
+    }
+    return "";
+}
+
+std::string suitToRank(Rank rank) {
+    switch (rank) {
+        case Rank::Two: return "Two";
+        case Rank::Three: return "Three";
+        case Rank::Four: return "Four";
+        case Rank::Five: return "Five";
+        case Rank::Six: return "Six";
+        case Rank::Seven: return "Seven";
+        case Rank::Eight: return "Eight";
+        case Rank::Nine: return "Nine";
+        case Rank::Ten: return "Ten";
+        case Rank::Jack: return "Jack";
+        case Rank::Queen: return "Queen";
+        case Rank::King: return "King";
+        case Rank::Ace: return "Ace";
+    }
+    return "";
+}
 
 class Deck {
 private:
-    std::pmr::vector<std::string> cards;
+    std::pmr::vector<Card> cards;
 public:
     Deck() {
-        std::pmr::vector<std::string> suits = {"Hearts", "Clubs", "Diamonds", "Spades"};
-        std::pmr::vector<std::string> ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen", "King", "Ace"};
 
-        for (int i = 0; i < suits.size(); i++) {
-            for (int j = 0; j < ranks.size(); j++) {
-                cards.push_back(ranks[j] + suits[i]);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                cards.push_back(Card{
+                    static_cast<Suit>(i),
+                    static_cast<Rank>(j)
+                });
             }
         }
     }
@@ -37,6 +82,10 @@ public:
         }
     }
     //I decided to use a Fisher-Yates shuffle over just randomly picking cards from an unshuffled deck cuz it felt cooler lol
+
+    void dealCards() {
+
+    }
 
 
 };
